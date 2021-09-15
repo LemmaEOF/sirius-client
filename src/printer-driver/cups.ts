a54518a64ca6b5cac316d79adf3918c618fe9154
Meredith Espinosa <6722691+LemmaEOF@users.noreply.github.com>
2021-09-15 19:51:10 +0000

    ...try the pnger from escpos

--- src/printer-driver/cups.ts
+++ src/printer-driver/cups.ts
@@ -2,27 +2,43 @@ import { PrinterDriverInterface, PrintingResult } from '.';
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
+import gm from 'gm';
 const exec = require("child_process").exec;
-const tga2png = require("tga2png");
+
+const im = gm.subClass({ imageMagick: true });
+
+const pnger = async (buf: Buffer): Promise<Buffer> => {
+  return new Promise((resolve, reject) => {
+    im(buf)
+      .colors(2)
+      .define('png:bit-depth=1')
+      .toBuffer('PNG', (err, out) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(out);
+      });
+  });
+};
 
 export default class CupsPrinterDriver implements PrinterDriverInterface {
     async print(buffer: Buffer): Promise<PrintingResult> {
-      return new Promise(resolve => {
+      return new Promise(async resolve => {
         const tempDir = path.join(os.tmpdir(), 'sirius-client');
         fs.mkdirSync(tempDir, { recursive: true });
   
         const tempFile = path.join(tempDir, 'to_print.bmp');
   
-        fs.writeFileSync(tempFile, buffer);
+        fs.writeFileSync(tempFile, await pnger(buffer));
         console.log(`Written: ${tempFile}`);
 
         const newTempFile = path.join(tempDir, 'to_print.png')
 
-        tga2png(tempFile, newTempFile);
         exec('lq ' + newTempFile);
         // const bitmap = bitmapify(buffer);
         //termImg(buffer);
-        resolve();
+        resolve(buffer);
       });
     }
   }
